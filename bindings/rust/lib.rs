c3cc8247c6d6d112e1768104ace8906b9fe3bcb3
Daniel Müller <deso@posteo.net>
2025-09-10 14:53:02 -0700

    Work with tree-sitter 0.22
    Adjust the crate to work with tree-sitter 0.22 for user that need this
    version.

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,9 +1,11 @@
 //! This crate provides BPF C language support for the [tree-sitter] parsing library.
 //!
-//! Typically, you will use the [`LANGUAGE`] constant to add this language to a
-//! tree-sitter [`Parser`], and then use the parser to parse some code:
+//! Typically, you will use the [language][language func] function to add this language to a
+//! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter::Parser;
+//!
 //! let code = r#"
 //!   SEC("kprobe/cap_capable")
 //!   int BPF_KPROBE(kprobe__cap_capable, const struct cred *cred,
@@ -11,38 +13,34 @@
 //!   	return record_cap(ctx, cred, targ_ns, cap, cap_opt);
 //!   }
 //! "#;
-//! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_bpf_c::LANGUAGE;
-//! parser
-//!     .set_language(&language.into())
-//!     .expect("Error loading BPF-C parser");
+//! let mut parser = Parser::new();
+//! parser.set_language(&tree_sitter_bpf_c::language()).expect("Error loading BPF C grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
 //!
-//! [`Parser`]: https://docs.rs/tree-sitter/0.25.6/tree_sitter/struct.Parser.html
+//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+//! [language func]: fn.language.html
+//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter_language::LanguageFn;
+use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_bpf_c() -> *const ();
+    fn tree_sitter_bpf_c() -> Language;
 }
 
-/// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_bpf_c) };
-
-/// The content of the [`node-types.json`] file for this grammar.
+/// Get the tree-sitter [Language][] for this grammar.
 ///
-/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers/6-static-node-types
+/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+pub fn language() -> Language {
+    unsafe { tree_sitter_bpf_c() }
+}
+
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
-
-// NOTE: uncomment these to include any queries that this grammar contains:
-
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -50,7 +48,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::LANGUAGE.into())
-            .expect("Error loading BPF-C parser");
+            .set_language(&super::language())
+            .expect("Error loading BPF C grammar");
     }
 }
